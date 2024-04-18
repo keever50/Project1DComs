3f92044069f3ba1e4184b9c715308d98db382374
Kevin Witteveen <kevinwit1999@gmail.com>
2024-04-18 10:42:31 +0200

    receive

--- src/main_test_receive.cpp
+++ src/main_test_receive.cpp
@@ -3,28 +3,41 @@
 #include <Arduino.h>
 #include <RH_ASK.h>
 
-RH_ASK driver(2000, 3, 4, 0);
+RH_ASK driver(2000, 3, 4, 0, false);
+
 
 void setup()
 {
-    Serial.begin(19200);
-    driver.init();
+    Serial.begin(9600);
+    bool success = driver.init();
+    if(!success)
+    {
+        Serial.println("init fail");
+        delay(500);
+        while(true);
+    }
+    //pinMode(3, INPUT);
+
 }
+
 
 void loop()
 {
     uint8_t len;
-    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buf[12];
+   // driver.printBuffer("buffer:", buf, 4);
+    // d
     if(driver.recv(buf, &len))
     {
         Serial.println(len);
         Serial.print("start[");
         for(int i=0;i<len;i++)
         {
-            Serial.print(buf[i]);
+            Serial.print((char)buf[i]);
         }
         Serial.println("]end");
     }
+    delay(100);
 }
 
 #endif
