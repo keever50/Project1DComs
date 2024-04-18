aa0672d14c94049a616cc81befe202e34daf4f28
Kevin Witteveen <kevinwit1999@gmail.com>
2024-04-18 12:53:54 +0200

    receive

--- lib/hu_protocol/hu_protocol.cpp
+++ lib/hu_protocol/hu_protocol.cpp
@@ -5,7 +5,17 @@ int hu_protocol_transmit(RH_ASK* driver, hu_packet_t* packet)
 
 }
 
-int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet)
+int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet, int buff_length)
 {
+    uint8_t len;
+    if(!driver->recv(hu_protocol_buffer, &len)) return 1;
+    
+    // Search for the start byte
+    
 
+}
+
+int hu_protocol_calculate_LRC(hu_packet_t* packet)
+{
+    return 0;
 }
--- lib/hu_protocol/hu_protocol.h
+++ lib/hu_protocol/hu_protocol.h
@@ -2,6 +2,12 @@
 #include <Arduino.h>
 #include <RH_ASK.h>
 
+#define HU_PROTOCOL_START_BYTE      0x01
+#define HU_PROTOCOL_END_BYTE        0x04
+
+#define HU_PROTOCOL_BUFFER_SIZE RH_ASK_MAX_MESSAGE_LEN
+uint8_t hu_protocol_buffer[HU_PROTOCOL_BUFFER_SIZE];
+
 typedef struct hu_packet_s
 {
     uint8_t start;
@@ -14,7 +20,8 @@ typedef struct hu_packet_s
     uint8_t LRC;
 }hu_packet_t;
 
-int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet);
+// takes a pointer to driver, packet and takes the max length of the data inside packet as buff_length. Returns 0 on success, 1 on no message and 2 on error
+int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet, int buff_length );
 int hu_protocol_transmit(RH_ASK* driver, hu_packet_t* packet);
 
 // Calculates the length of a packet and returns the value
