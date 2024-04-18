99d10ffcce469053249dcc4a8c0a05f2823d83ec
Kevin Witteveen <kevinwit1999@gmail.com>
2024-04-18 10:08:53 +0200

    Transmit

--- src/main_test_receive.cpp
+++ src/main_test_receive.cpp
@@ -1,15 +1,30 @@
 #ifdef TEST_RECEIVE
 
 #include <Arduino.h>
+#include <RH_ASK.h>
+
+RH_ASK driver(2000, 3, 4, 0);
 
 void setup()
 {
-
+    Serial.begin(19200);
+    driver.init();
 }
 
 void loop()
 {
-    
+    uint8_t len;
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    if(driver.recv(buf, &len))
+    {
+        Serial.println(len);
+        Serial.print("start[");
+        for(int i=0;i<len;i++)
+        {
+            Serial.print(buf[i]);
+        }
+        Serial.println("]end");
+    }
 }
 
 #endif
--- src/main_test_transmitter.cpp
+++ src/main_test_transmitter.cpp
@@ -1,16 +1,19 @@
 #ifdef TRANSMIT_TEST
 
-#include<RadioHead.h>
+#include <RH_ASK.h>
+
+RH_ASK driver(2000, 3, 4, 0, false);
 
 
 void setup()
 {
-
+    driver.init();
 }
 
 void loop()
 {
-    
+    driver.send("test",4);
+    delay(100);
 }
 
 
