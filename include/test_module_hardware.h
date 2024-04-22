6565ed0c52414168e590555d204a015cc332e31b
kevin <kevinwit1999@gmail.com>
2024-04-22 19:33:44 +0200

    TM hardware defines

---
+++ include/test_module_hardware.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/****************
+     RADIO
+****************/
+// Pins
+#define RA_BITRATE          1000
+#define RA_RX               3
+#define RA_TX               9
+
+/****************
+    TFT screen
+****************/
+// Pins
+#define TFT_RST 8
+#define TFT_RS  7
+#define TFT_CS  10  // SS
+#define TFT_SDI 11  // MOSI
+#define TFT_CLK 13  // SCK
+#define TFT_LED 5   // Backlight
+// Settings
+#define TFT_BRIGHTNESS 200 // Initial brightness of TFT backlight
