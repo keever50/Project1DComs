f0648a3353077d316ec7ff9bc451b21a179c1865
Kevin Witteveen <kevinwit1999@gmail.com>
2024-04-22 18:23:48 +0200

    protocol

--- lib/hu_protocol/hu_protocol.cpp
+++ lib/hu_protocol/hu_protocol.cpp
@@ -1,4 +1,5 @@
 #include <hu_protocol.h>
+#include <biterrors.h>
 
 int hu_protocol_transmit(RH_ASK* driver, hu_packet_t* packet)
 {
@@ -23,17 +24,27 @@ int hu_protocol_transmit(RH_ASK* driver, hu_packet_t* packet)
     return 0;
 }
 
+int hu_protocol_find_start_pos()
+{
+    // Search for start byte
+    for(int pos=0;pos<HU_PROTOCOL_BUFFER_SIZE-HU_PROTOCOL_MAX_PACKET_SIZE;pos++)
+    {
+        if(hu_protocol_buffer[pos]==HU_PROTOCOL_START_BYTE)
+        {
+            return pos;
+        }
+    }
+    return -1;
+}
+
 int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet, int buff_length)
 {
     uint8_t len;
     if(!driver->recv(hu_protocol_buffer, &len)) return 1;
     
     int i=0;
-    // Search for start byte
-    for(int pos=0;pos<HU_PROTOCOL_BUFFER_SIZE;pos++)
-    {
-
-    }
+    i=hu_protocol_find_start_pos();
+    if(i<0){return 1;}
 
     // Construct packet
     packet->start=hu_protocol_buffer[i++];
@@ -42,8 +53,55 @@ int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet, int buff_length)
     packet->source=hu_protocol_buffer[i++];
     packet->destination=hu_protocol_buffer[i++];
 
-    // Do some checks before getting the data
+    // Check whether the destination is us. If not, return 1
 
+
+    // Do some checks before getting the data
+    if(packet->length>HU_PROTOCOL_MAX_PACKET_SIZE)
+    {
+        Serial.println("receive packet error: packet too long. Length=");
+        Serial.print(packet->function);
+        Serial.println("");
+        return 2;
+    }
+
+    if(packet->function>HU_PROTOCOL_FUNCTION_RANGE)
+    {
+        Serial.print("receive packet error: unknown function [");
+        Serial.print(packet->function);
+        Serial.println("]");
+        return 2;
+    }
+
+    // Transfer the data
+    memcpy(packet->data, hu_protocol_buffer+i, packet->length-HU_PROTOCOL_MIN_PACKET_LEN);
+    i=i+(packet->length-HU_PROTOCOL_MIN_PACKET_LEN);
+
+    // Get the last bytes
+    packet->end=hu_protocol_buffer[i++];
+    packet->LRC=hu_protocol_buffer[i++];
+
+    // Last checks
+    if(packet->end==HU_PROTOCOL_END_BYTE)
+    {
+        Serial.print("receive packet error: incorrect packet end [");
+        Serial.print(packet->end);
+        Serial.println("]");       
+        return 2;
+    }
+
+    uint8_t LRC = get_LRC((uint8_t*)packet, packet->length-1);
+    if(LRC!=packet->LRC)
+    {
+        Serial.print("receive packet error: incorrect LRC [");
+        Serial.print(LRC);
+        Serial.print("] should be [");   
+        Serial.print(packet->LRC);
+        Serial.println("]");      
+        return 2;     
+    }
+
+    return 0;
 
 }
 
--- lib/hu_protocol/hu_protocol.h
+++ lib/hu_protocol/hu_protocol.h
@@ -4,6 +4,10 @@
 
 #define HU_PROTOCOL_START_BYTE      0x01
 #define HU_PROTOCOL_END_BYTE        0x04
+#define HU_PROTOCOL_MAX_PACKET_SIZE 0x28
+#define HU_PROTOCOL_MAX_DATA_SIZE   36
+#define HU_PROTOCOL_FUNCTION_RANGE  13
+#define HU_PROTOCOL_MIN_PACKET_LEN  7
 
 #define HU_PROTOCOL_BUFFER_SIZE RH_ASK_MAX_MESSAGE_LEN
 uint8_t hu_protocol_buffer[HU_PROTOCOL_BUFFER_SIZE];
@@ -15,12 +19,18 @@ typedef struct hu_packet_s
     uint8_t function;
     uint8_t source;
     uint8_t destination;
-    uint8_t* data;
+    uint8_t data[HU_PROTOCOL_MAX_DATA_SIZE];
     uint8_t end;
     uint8_t LRC;
 }hu_packet_t;
 
-// takes a pointer to driver, packet and takes the max length of the data inside packet as buff_length. Returns 0 on success, 1 on no message and 2 on error
+/*
+    Checks if we received a packet.
+    Returns 0 on we received a packet correctly,
+    returns 1 on we received nothing,
+    returns 2 on we received a corrupted packet
+
+*/
 int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet, int buff_length );
 int hu_protocol_transmit(RH_ASK* driver, hu_packet_t* packet);
 
