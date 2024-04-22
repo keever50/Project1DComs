e9e7baf831aa3405c9af6da8a703c3509f1d7bf8
Kevin Witteveen <kevinwit1999@gmail.com>
2024-04-22 15:30:58 +0200

    reggergfd

--- lib/hu_protocol/hu_protocol.cpp
+++ lib/hu_protocol/hu_protocol.cpp
@@ -11,6 +11,7 @@ int hu_protocol_transmit(RH_ASK* driver, hu_packet_t* packet)
     if(!driver->send(packet->data, packet->length)) return 6;
     if(!driver->send(&packet->end,1)) return 7;
     if(!driver->send(&packet->LRC,1)) return 8;
+    
 }
 
 int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet, int buff_length)
@@ -18,8 +19,22 @@ int hu_protocol_receive(RH_ASK* driver, hu_packet_t* packet, int buff_length)
     uint8_t len;
     if(!driver->recv(hu_protocol_buffer, &len)) return 1;
     
-    // Search for the start byte
-    
+    int i=0;
+    // Search for start byte
+    for(int pos=0;pos<HU_PROTOCOL_BUFFER_SIZE;pos++)
+    {
+
+    }
+
+    // Construct packet
+    packet->start=hu_protocol_buffer[i++];
+    packet->length=hu_protocol_buffer[i++];
+    packet->function=hu_protocol_buffer[i++];
+    packet->source=hu_protocol_buffer[i++];
+    packet->destination=hu_protocol_buffer[i++];
+
+    // Do some checks before getting the data
+
 
 }
 
--- src/main_test_receive.cpp
+++ src/main_test_receive.cpp
@@ -18,6 +18,7 @@ void setup()
     }
     //pinMode(3, INPUT);
 
+
 }
 
 
