51381a8087c0d49884999dd9a78f00026edbd5b0
kevin <kevinwit1999@gmail.com>
2024-04-25 21:48:28 +0200

    TFT

--- include/test_module_hardware.h
+++ include/test_module_hardware.h
@@ -4,19 +4,19 @@
      RADIO
 ****************/
 // Pins
-#define RA_BITRATE          1000
+#define RA_BITRATE          500
 #define RA_RX               3
-#define RA_TX               11
+#define RA_TX               5
 #define RA_TR               2
 /****************
     TFT screen
 ****************/
 // Pins
-#define TFT_RST 8
+#define TFT_RST 9
 #define TFT_RS  7
 #define TFT_CS  10  // SS
 #define TFT_SDI 11  // MOSI
 #define TFT_CLK 13  // SCK
-#define TFT_LED 5   // Backlight
+#define TFT_LED 8   // Backlight
 // Settings
 #define TFT_BRIGHTNESS 200 // Initial brightness of TFT backlight
