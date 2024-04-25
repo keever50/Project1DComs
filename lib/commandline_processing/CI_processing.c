851886c0a1af4d4a18d5cae99455ad297638dafb
kevin <kevinwit1999@gmail.com>
2024-04-25 21:48:36 +0200

    terminal

--- lib/commandline_processing/CI_processing.c
+++ lib/commandline_processing/CI_processing.c
@@ -3,9 +3,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Standard memcpy replacement
+void memcopy( char* dest, const char* src, int amount )
+{
+    for(int i=0;i<amount;i++)
+    {
+        dest[i]=src[i];
+    }
+}
 
+void cli_put( cli_terminal_t* term, uint16_t x, uint16_t y, char c )
+{
+    uint16_t addr = y*term->width+x;
+    if(addr>=term->buffersize) return;
+    term->buffer[addr]=c;
+    term->colors[addr]=term->current_color;
+}
 
-unsigned char get_next_argument_iterative(int* iterator, const char* str, char* arg, int max_arg_len, int* new_arg_len )
+void cli_shift_up( cli_terminal_t* term )
+{
+    // For each line copy next line up. Except the first line
+    for(uint16_t addr=term->width ; addr<term->buffersize ; addr=addr+term->width)
+    {
+        memcopy(term->buffer+(addr-term->width), term->buffer+addr, term->width);
+        memcopy(term->colors+(addr-term->width), term->colors+addr, term->width);
+    }
+
+    // Clean the new line appeared on the bottom
+    uint16_t top = term->buffersize-term->width;
+    memset(term->buffer+top, 0, term->width);
+    memset(term->colors+top, 0, term->width);
+}
+
+unsigned char cli_get_next_argument_iterative(int* iterator, const char* str, char* arg, int max_arg_len, int* new_arg_len )
 {
     //Clear argument
     memset(arg, 0, max_arg_len);
--- lib/commandline_processing/CI_processing.h
+++ lib/commandline_processing/CI_processing.h
@@ -1,6 +1,26 @@
 #ifndef CI_PROC_H
 #define CI_PROC_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+typedef struct
+{
+    uint16_t width;
+    uint16_t height;
+    uint16_t buffersize;
+    char current_color;
+    char* buffer;
+    char* colors;
+} cli_terminal_t;
+
+void cli_put( cli_terminal_t* term, uint16_t x, uint16_t y, char c );
+void cli_shift_up( cli_terminal_t* term );
+
+
 /*
 This function can be repeated to find all the arguments in a string.
 
@@ -12,6 +32,10 @@ int* new_arg_len is where the size of the new argument we found will be written 
 
 Returns 0 when we reached the last argument, otherwise 1.
 */
-unsigned char get_next_argument_iterative(int* iterator, const char* str, char* arg, int max_arg_len, int* new_arg_len );
+unsigned char cli_get_next_argument_iterative(int* iterator, const char* str, char* arg, int max_arg_len, int* new_arg_len );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
