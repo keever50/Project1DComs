f14cfb960519b9c71f38576f88e1b61d4dde389e
hayan <hayan.rafa3@gmail.com>
2024-04-30 13:34:32 +0200

    kev2

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -26,6 +26,94 @@ int thermistorIndex = 0;
 int ldrIndex = 0;
 int potIndex = 0;
 
+
+void light() 
+{
+LDRvalue = analogRead(LDRPin);
+storeReading(LDRvalue, ldrReadings, &ldrIndex);
+}
+
+void Temp() 
+{
+Vo = analogRead(ThermistorPin);
+R2 = R1 * (1023.0 / (float)Vo - 1.0);
+logR2 = log(R2);
+T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+T = T - 273.15;
+storeReading((int)T, thermistorReadings, &thermistorIndex);
+}
+
+void pot() 
+{
+int sensorValue = analogRead(A2);
+storeReading(sensorValue, potReadings, &potIndex);
+}
+
+void storeReading(int value, int* readings, int* index) 
+{
+readings[*index] = value;
+*index = (*index + 1) % numReadings; // Move to the next index, wrapping around if necessary
+}
+
+void calculateStats(int* readings, int* max, int* min, float* avg) 
+{
+*max = readings[0];
+*min = readings[0];
+*avg = 0;
+  
+for (int i = 0; i < numReadings; i++)
+{
+if (readings[i] > *max) {
+  *max = readings[i];
+}
+if (readings[i] < *min) {
+  *min = readings[i];
+}
+*avg += readings[i];
+}  
+*avg /= numReadings;
+}
+
+void printStats() 
+{
+int maxTemp, minTemp;
+float avgTemp;
+calculateStats(thermistorReadings, &maxTemp, &minTemp, &avgTemp);
+  
+int maxLDR, minLDR;
+float avgLDR;
+calculateStats(ldrReadings, &maxLDR, &minLDR, &avgLDR);
+  
+int maxPot, minPot;
+float avgPot;
+calculateStats(potReadings, &maxPot, &minPot, &avgPot);
+  
+Serial.println("Temperature:");
+Serial.print("Max: ");
+Serial.println(maxTemp);
+Serial.print("Min: ");
+Serial.println(minTemp);
+Serial.print("Avg: ");
+Serial.println(avgTemp);
+  
+Serial.println("LDR:");
+Serial.print("Max: ");
+Serial.println(maxLDR);
+Serial.print("Min: ");
+Serial.println(minLDR);
+Serial.print("Avg: ");
+Serial.println(avgLDR);
+  
+Serial.println("Potentiometer:");
+Serial.print("Max: ");
+Serial.println(maxPot);
+Serial.print("Min: ");
+Serial.println(minPot);
+Serial.print("Avg: ");
+Serial.println(avgPot);
+Serial.println();
+}
+
 void setup()
 {
 Serial.begin(9600);
@@ -33,7 +121,14 @@ Serial.begin(9600);
 
 void loop()
 {
-    
+for (int i = 0; i < numReadings; i++) 
+{
+Temp();
+light();
+pot();
+printStats(); // Print statistics after collecting 10 readings
+delay(1000); // Delay before taking the next set of readings
+}
 }
 
 #endif
